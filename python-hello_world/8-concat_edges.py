b6e0a3938561066d84318c73fa90ef842a27e654
Jean de Dieu Muhirwa Harerimana <156063018+JDMUHIRWA@users.noreply.github.com>
2024-02-22 08:48:13 +0000

    task 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax
- print(f"object-oriented programming with Python" {str})
+str = "Python is an interpreted, interactive, object-oriented programming\
+language that combines remarkable power with very clear syntax"
+str = [39:66] + str[107:112] + str[:6]
+print(str)
