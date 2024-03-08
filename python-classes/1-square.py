be8eabc57b775281a46cb3b6cf1bd7c2947b4e85
Jean de Dieu Muhirwa Harerimana <156063018+JDMUHIRWA@users.noreply.github.com>
2024-03-08 09:53:47 +0000

    task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,5 +6,5 @@
 class Square:
     """Private instance attribute: size
     Instantiation with size (no type/value verification)"""
-    def __init__(self,size):
-        self.size = size
+    def __init__(self, size):
+        self.__size = size
