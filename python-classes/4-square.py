09bc8e1cc934ca2d4d7e9ce2535e7c2614af94ca
Jean de Dieu Muhirwa Harerimana <156063018+JDMUHIRWA@users.noreply.github.com>
2024-03-08 11:08:15 +0000

    task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" Write a class that defines a square"""
+"""Write a class Square that defines a square"""
 
 
-class User:
-    """The class defines a square"""
+class Square:
+    """Write a class Square that defines a square"""
 
     def __init__(self, size=0):
         self.size = size
@@ -15,7 +15,7 @@ class User:
     @size.setter
     def size(self, value):
         if type(value) is not int:
-            raise TypeError("size must be an int")
+            raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
