34d6beefe9fb53afa23dcc8ba6170db9ec0f9dcb
Jean de Dieu Muhirwa Harerimana <156063018+JDMUHIRWA@users.noreply.github.com>
2024-03-19 19:35:58 +0000

    task 7 adding com

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,12 +1,3 @@
---this is my script 
---about inserting a new row in the first table
-# Get the database name from the command line argument
-db_name="$1"
-# Check if the database name was provided
-if [ -z "$db_name" ]; then
-    echo "Error: Database name not provided"
-    exit 1
-fi
-
-# Insert the new row into the first_table table
-mysql -e "USE $db_name; INSERT INTO first_table (id, name) VALUES (89, 'Best School');"
+-- A script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server
+-- Query to insert a new row
+INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');
