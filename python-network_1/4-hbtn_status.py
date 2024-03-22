2ac87708113f55f3f2a6d216344b914c5c58ba77
Jean de Dieu Muhirwa Harerimana <156063018+JDMUHIRWA@users.noreply.github.com>
2024-03-22 10:08:36 +0000

    module

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,9 +1,13 @@
+
 #!/usr/bin/python3
-"""Python script that fetches an URL with requests package"""
+"""
+Python script that fetches an URL with requests package
+"""
 import requests
 
 
 if __name__ == "__main__":
     r = requests.get('https://intranet.hbtn.io/status')
     t = r.text
-    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t)
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
+
